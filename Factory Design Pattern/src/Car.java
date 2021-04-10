991450a8fa8b902949abbead20ba68f9d3638bc3
1InfinityDoesExist <patelavinash508@gmail.com>
2021-04-10 20:51:00 +0545

    factory design pattern

---
+++ Factory Design Pattern/src/Car.java
@@ -0,0 +1,15 @@
+
+public class Car extends Vehicle {
+
+	int wheel;
+
+	public Car(int wheel) {
+		this.wheel = wheel;
+	}
+
+	@Override
+	public int getWheel() {
+		return this.wheel;
+	}
+
+}
---
+++ Factory Design Pattern/src/Test.java
@@ -0,0 +1,10 @@
+
+public class Test {
+	public static void main(String[] args) {
+		Vehicle car = VehicleFactory.getInstance("car", 4);
+		System.out.println(car);
+
+		Vehicle bike = VehicleFactory.getInstance("bike", 2);
+		System.out.println(bike);
+	}
+}
